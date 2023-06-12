5b1b772794812e86dc5647c938266a6c3f14b711
Anton-Sakharov-hub <saharovanton357@gmail.com>
2023-06-12 18:25:43 +0300

    create FriendList component
     - add styles;
     - add friend data;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
+import FriendsList from './FriendsList/FriendsList';
 import user from '../data/user.json';
 import stats from '../data/statistics.json';
+import friends from "../data/friends.json"
 
 export const App = () => {
   return (
     <>
       <Profile user={user} />
       <Statistics stats={stats} title="UPLOAD STATS" />
-      <Statistics stats={stats}  />
+      <Statistics stats={stats} />
+      <FriendsList friends={friends}/>
     </>
   );
 };
---
+++ src/components/FriendsList/FriendsList.jsx
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+import styles from './FriendsList.module.css';
+
+function FriendsList({ friends }) {
+  return (
+    <section className={styles.wrap}>
+      <ul className={styles.friendsList}>
+        {friends?.map(({ id, avatar, name, isOnline }) => (
+          <li className={styles.item} key={id}>
+            <span className={styles.status} style={{backgroundColor: isOnline ? "green" : "red"}}></span>
+            <img className={styles.avatar} src={avatar} alt={name} />
+            <p className={styles.name}>{name}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+FriendsList.propTypes = {
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  isOnline: PropTypes.string,
+};
+
+export default FriendsList;
+
+// [
+//   {
+//     "avatar": "https://cdn-icons-png.flaticon.com/512/1998/1998592.png",
+//     "name": "Mango",
+//     "isOnline": true,
+//     "id": 1812
+//   },
+//   {
+//     "avatar": "https://cdn-icons-png.flaticon.com/512/616/616438.png",
+//     "name": "Kiwi",
+//     "isOnline": false,
+//     "id": 1137
+//   },
+//   {
+//     "avatar": "https://cdn-icons-png.flaticon.com/512/1623/1623681.png",
+//     "name": "Ajax",
+//     "isOnline": true,
+//     "id": 1213
+//   },
+//   {
+//     "avatar": "https://cdn-icons-png.flaticon.com/512/2977/2977285.png",
+//     "name": "Jay",
+//     "isOnline": true,
+//     "id": 1714
+//   },
+//   {
+//     "avatar": "https://cdn-icons-png.flaticon.com/512/1998/1998749.png",
+//     "name": "Poly",
+//     "isOnline": false,
+//     "id": 1284
+//   }
+// ]
