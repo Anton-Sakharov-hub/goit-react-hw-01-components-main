31a8b41303a4faad1c7af733a027a3acf6ec7f3e
Anton-Sakharov-hub <saharovanton357@gmail.com>
2023-06-13 15:42:54 +0300

    create TransactionHistory component
     - add component;
     - add styles;
     - add data transanctions;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendsList from './FriendsList/FriendsList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 import user from '../data/user.json';
 import stats from '../data/statistics.json';
-import friends from "../data/friends.json"
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -11,7 +13,8 @@ export const App = () => {
       <Profile user={user} />
       <Statistics stats={stats} title="UPLOAD STATS" />
       <Statistics stats={stats} />
-      <FriendsList friends={friends}/>
+      <FriendsList friends={friends} />
+      <TransactionHistory transactions={transactions}/>
     </>
   );
 };
